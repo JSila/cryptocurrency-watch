b7b094345fbdb6e358a1edd0d924567c0a46ba74
Jernej Sila <jernej.sila@gmail.com>
2018-04-01 20:23:23 +0200

    adds tests for api middleware

--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -6,14 +6,12 @@ export default (httpClient, history) => store => dispatch => action => {
     const res = dispatch(action)
 
     if (action.type === CRYPTO_CURRENCIES_FETCH) {
-        httpClient.get('/?' + qs.stringify(action.payload)).then(res => {
+        httpClient.get("/?" + qs.stringify(action.payload)).then(res => {
             dispatch(setCryptoCurrencies({
                 data: res.data,
                 fiat: action.payload.convert
             }))
 
-            // ideally this should be in it's own middleware because of single responsibility principle
-            // but for now it's ok given it's just one line of code
             history.push({search: '?' + qs.stringify({ fiat: action.payload.convert })})
         })
     }
